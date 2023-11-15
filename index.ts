73d35f47d2bda01ce959e2c5e3a70557380c9adf
Dominik Schorn <dominik.schorn@nordlb.de>
2023-11-15 11:09:56 +0100

    feat: use npx for running npm commands

--- index.ts
+++ index.ts
@@ -1,21 +1,31 @@
-import * as grafana from "@lbrlabs/pulumi-grafana";
-import * as pulumi from "@pulumi/pulumi";
+import * as grafana from '@lbrlabs/pulumi-grafana'
+import * as pulumi from '@pulumi/pulumi'
 
 export class Grafana extends pulumi.ComponentResource {
-    grafanaOrg: grafana.Organization;
+    grafanaOrg: grafana.Organization
 
-        constructor(orgName: string, opts: any, admins?: string[], editors?: string[], viewers?: string[]) {
-        super("dominike93:project:Grafana", orgName, {}, opts);
+    constructor(
+        orgName: string,
+        opts: any,
+        admins?: string[],
+        editors?: string[],
+        viewers?: string[]
+    ) {
+        super('dominike93:project:Grafana', orgName, {}, opts)
 
-        this.grafanaOrg = new grafana.Organization(orgName, {
-            name: orgName,
-            admins: admins,
-            editors: editors,
-            viewers: viewers
-        }, { parent: this });
+        this.grafanaOrg = new grafana.Organization(
+            orgName,
+            {
+                name: orgName,
+                admins: admins,
+                editors: editors,
+                viewers: viewers,
+            },
+            { parent: this }
+        )
 
         this.registerOutputs({
             grafanaOrg: this.grafanaOrg,
-        });
+        })
     }
 }
